60dee20e4b424c43bdfaea5ccc9bd07435153b5c
Jessy <jessy-walker@hotmail.com>
2022-12-02 23:03:28 -0500

    Added delay logic.

--- lua/autorun/splintercell_nvg.lua
+++ lua/autorun/splintercell_nvg.lua
@@ -8,6 +8,10 @@ CreateClientConVar("SPLINTERCELL_NVG_CYCLE", "23", true, true, "Which key to cyc
 -- Setup toggle and cycle commands for goggles.
 CreateClientConVar("SPLINTERCELL_NVG_TOGGLE", "0", false, false);
 
+-- Constants used to delay player input using the goggles to avoid epilepsy.
+local __ToggleDelay = 1.5;
+local __SwitchDelay = 0.5;
+
 --!
 --! @brief      Internal function. Creates the necessary network integers on the client.
 --!
@@ -22,6 +26,14 @@ local function __SetupDefaults(player)
 	if (player:GetNWInt("SPLINTERCELL_NVG_LAST_GOGGLE", 0) == 0) then
 		player:SetNWInt("SPLINTERCELL_NVG_LAST_GOGGLE", 1);
 	end
+
+	if (player:GetNWFloat("SPLINTERCELL_NVG_NEXT_TOGGLE", 0) == 0) then
+		player:SetNWFloat("SPLINTERCELL_NVG_NEXT_TOGGLE", CurTime());
+	end
+
+	if (player:GetNWFloat("SPLINTERCELL_NVG_NEXT_SWITCH", 0) == 0) then
+		player:SetNWFloat("SPLINTERCELL_NVG_NEXT_SWITCH", CurTime());
+	end
 end
 
 --! 
@@ -34,10 +46,16 @@ hook.Add("PlayerButtonDown", "SPLINTERCELL_NVG_INPUT", function(player, button)
 	-- Stop current goggle reference as the last one used.
 	local toggle = GetConVar("SPLINTERCELL_NVG_TOGGLE"):GetBool();
 	local current = player:GetNWInt("SPLINTERCELL_NVG_CURRENT_GOGGLE");
+
+	-- Timing data.
+	local nextToggle = player:GetNWInt("SPLINTERCELL_NVG_NEXT_TOGGLE");
+	local nextSwitch = player:GetNWInt("SPLINTERCELL_NVG_NEXT_SWITCH");
+
+	-- Current configuration.
 	local goggle = SPLINTERCELL_NVG_CONFIG[current];
 
 	-- Toggle on and off.
-	if (button == GetConVar("SPLINTERCELL_NVG_INPUT"):GetInt()) then
+	if (button == GetConVar("SPLINTERCELL_NVG_INPUT"):GetInt() && CurTime() > nextToggle) then
 
 		-- Emit sound on toggle on/off based on the goggle's config.
 		if (!toggle) then
@@ -48,10 +66,11 @@ hook.Add("PlayerButtonDown", "SPLINTERCELL_NVG_INPUT", function(player, button)
 
 		-- Send out command to toggle the goggles.
 		player:ConCommand("SPLINTERCELL_NVG_TOGGLE " .. (!toggle && 1 || 0));
+		player:SetNWFloat("SPLINTERCELL_NVG_NEXT_TOGGLE", CurTime() + __ToggleDelay);
 	end
 
 	-- Cycle between different goggle modes.
-	if (toggle && button == GetConVar("SPLINTERCELL_NVG_CYCLE"):GetInt()) then
+	if (toggle && button == GetConVar("SPLINTERCELL_NVG_CYCLE"):GetInt() && CurTime() > nextSwitch) then
 
 		player:SetNWInt("SPLINTERCELL_NVG_LAST_GOGGLE", current);
 
@@ -59,5 +78,6 @@ hook.Add("PlayerButtonDown", "SPLINTERCELL_NVG_INPUT", function(player, button)
 		current = current + 1;
 		if (current > #SPLINTERCELL_NVG_CONFIG) then current = 1; end
 		player:SetNWInt("SPLINTERCELL_NVG_CURRENT_GOGGLE", current);
+		player:SetNWFloat("SPLINTERCELL_NVG_NEXT_SWITCH", CurTime() + __SwitchDelay);
 	end
 end);
