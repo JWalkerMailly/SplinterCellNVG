5e798f8de115ad1bdde08fa7191c0c6b43411447
Jessy <jessy-walker@hotmail.com>
2022-12-05 20:16:15 -0500

    Test

--- lua/autorun/splintercell_nvg.lua
+++ lua/autorun/splintercell_nvg.lua
@@ -4,47 +4,33 @@
 --!
 hook.Add("PlayerButtonDown", "SPLINTERCELL_NVG_INPUT", function(player, button)
 
-	-- Server only code. If whitelist system is on, make sure the player is whitelisted before.
+	-- Server only code.
+	if (!SERVER) then return; end
+
+	-- If whitelist system is on, make sure the player is whitelisted before.
 	-- The only way to bypass the whitelist if it is on, is if the player already has his
 	-- goggles active while his allowed goggles list changed, in that case, we allow to run
 	-- to prevent him getting stuck in his active goggles.
-	if (!SERVER) then return; end
-	if (_G:SCNVG_IsWhitelistOn() && !player:SCNVG_IsWhitelisted() && !player:SCNVG_IsGoggleActive()) then return; end
+	local whitelist = _G:SCNVG_IsWhitelistOn();
+	if (whitelist && !player:SCNVG_IsWhitelisted() && !player:SCNVG_IsGoggleActive()) then return; end
 
 	-- If not already done, prepare networking data on the player.
 	player:SCNVG_SetupNetworking();
 
 	-- Toggle goggle on/off.
 	if (player:SCNVG_CanToggleGoggle(button)) then
+
+		if (whitelist) then
+			player:SetBodygroup(1, 0);
+			player:AnimRestartGesture(GESTURE_SLOT_CUSTOM, ACT_ARM, true);
+		end
+
 		player:SCNVG_ToggleGoggle();
 	end
 
 	-- Cycle between different goggle modes.
 	if (player:SCNVG_CanSwitchGoggle(button)) then
 		player:SCNVG_SwitchToNextGoggle();
-	end
-end);
-
---! 
---! Simple hook to handle goggle third person animations when toggling on/off.
---!
-hook.Add("CalcMainActivity", "SPLINTERCELL_NVG_ANIMATIONS", function(player, velocity)
-
-	-- This hook only belongs to Splinter Cell player models.
-	if (!player:SCNVG_IsWhitelisted()) then return; end
-
-	local toggled = player:SCNVG_IsGoggleActive();
-
-	-- Goggles down animation.
-	if (toggled && !player:SCNVG_CanToggleGoggle()) then
-		player:SetBodygroup(1, 0);
-		return ACT_ARM, -1;
-	end
-
-	-- Goggles up animation.
-	if (!toggled && !player:SCNVG_CanToggleGoggle()) then
-		player:SetBodygroup(1, 1);
-		return ACT_DISARM, -1;
 	end
 end);
 
